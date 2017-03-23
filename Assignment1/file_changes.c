5ad6f5812f9ab5774ae27fcb5331e47d5e3d86cb
Timothy Barnard <timothy.barnard@mydit.ie>
2017-03-23 20:32:24 +0000

    daemon update35

--- Assignment1/file_changes.c
+++ Assignment1/file_changes.c
@@ -1,48 +1,201 @@
-/*This is the sample program to notify us for the file creation and file deletion takes place in “/tmp” directory*/
+/* Using Inotify to monitor the sub-dirs under the specifiied dir*/
+ 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <linux/inotify.h>
+#include <dirent.h>
+#include <limits.h>
+ 
+#define MAX_LEN 1024 /*Path length for a directory*/
+#define MAX_EVENTS 1024 /*Max. number of events to process at one go*/
+#define LEN_NAME 16 /*Assuming that the length of the filename won't exceed 16 bytes*/
+#define EVENT_SIZE  ( sizeof (struct inotify_event) ) /*size of one event*/
+#define BUF_LEN     ( MAX_EVENTS * ( EVENT_SIZE + LEN_NAME )) /*buffer to store the data of events*/
+ 
+ 
+/* Log file*/
+FILE *fp_log;
 
-#include "file_changes.h"
-#include "logger.h"
+static void skeleton_daemon() {
+	pid_t pid;
 
-void check_folder( char * directory_name, int watcher, int notifyID ) 
+	/* Fork off the parent process */
+	pid = fork();
+
+	/* An error occurred */
+	if (pid < 0){
+		//printf("pid<0 error");
+		exit(EXIT_FAILURE);
+	}
+	/* Success: Let the parent terminate */
+	if (pid > 0){
+		//printf("pid>0 parent terminate");
+		exit(EXIT_SUCCESS);
+	}
+
+	/* On success: The child process becomes session leader */
+	if (setsid() < 0){
+		//printf("setsid <0");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Catch, ignore and handle signals */
+	//TODO: Implement a working signal handler */
+	signal(SIGCHLD, SIG_IGN);
+	signal(SIGHUP, SIG_IGN);
+
+	/* Fork off for the second time*/
+	pid = fork();
+
+	/* An error occurred */
+	if (pid < 0){
+		//printf("pid<0");
+		exit(EXIT_FAILURE);
+	}
+	/* Success: Let the parent terminate */
+	if (pid > 0){
+		//printf("pid > 0 ");
+		exit(EXIT_SUCCESS);
+	}
+	/* Set new file permissions */
+	umask(0);
+
+	/* Change the working directory to the root directory */
+	/* or another appropriated directory */
+	chdir("/");
+
+	/* Close all open file descriptors */
+	int x;
+	for (x = sysconf(_SC_OPEN_MAX); x>0; x--)
+	{
+		close (x);
+	}
+}
+ 
+/* Add inotify watches to directories immediately under root
+ * in addition to itself */
+ 
+void add_watches(int fd, char *root)
+{
+  int wd;
+  char *abs_dir;
+  struct dirent *entry;
+  DIR *dp;
+ 
+  dp = opendir(root);
+  if (dp == NULL)
+    {
+      perror("Error opening the starting directory");
+      exit(0);
+    }
+ 
+  /* add watch to starting directory */
+  wd = inotify_add_watch(fd, root, IN_CREATE | IN_MODIFY | IN_DELETE);
+  if (wd == -1)
+    {
+      fprintf(fp_log,"Couldn't add watch to %s\n",root);
+    }
+  else
+    {
+      printf("Watching:: %s\n",root);
+    }
+ 
+  /* Add watches to the Level 1 sub-dirs*/
+  abs_dir = (char *)malloc(MAX_LEN);
+  while((entry = readdir(dp)))
+    {
+      /* if its a directory, add a watch*/
+      if (entry->d_type == DT_DIR)
+        {
+          strcpy(abs_dir,root);
+          strcat(abs_dir,entry->d_name);
+           
+          wd = inotify_add_watch(fd, abs_dir, IN_CREATE | IN_MODIFY | IN_DELETE);
+          if (wd == -1)
+              printf("Couldn't add watch to the directory %s\n",abs_dir);
+          else
+            printf("Watching:: %s\n",abs_dir);
+        }
+    }
+   
+  closedir(dp);
+  free(abs_dir);
+}
+ 
+/* Main routine*/
+int main( int argc, char **argv )
 {
   int length, i = 0;
-  char buffer[EVENT_BUF_LEN];
+  int fd;
+  char buffer[BUF_LEN], root[MAX_LEN];
+        
+   strcpy(root,"/root/logs");     
+    
+ 
+  /* Set up logger*/
+  fp_log = fopen("inotify_logger.log","a");
+  if (fp_log == NULL)
+    {
+      printf("Error opening logger. All output will be redirected to the stdout\n");
+      fp_log = stdout;
+    }
+ 
+  fd = inotify_init();
+  if ( fd < 0 ) {
+    perror( "Couldn't initialize inotify");
+  }
+ 
+  /* Read the sub-directories at one level under argv[1]
+   * and monitor them for access */
+  add_watches(fd,root);
 
-  length = read( notifyID, buffer, EVENT_BUF_LEN ); 
+  skeleton_daemon();
 
-  /*checking for error*/
-  if ( length < 0 ) {
-    perror( "read" );
-  }  
-
-  /*actually read return the list of change events happens. Here, read the change event one by one and process it accordingly.*/
-  while ( i < length ) {  struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];     if ( event->len ) {
-      if ( event->mask & IN_CREATE ) {
-        if ( event->mask & IN_ISDIR ) {
-          //printf( "New directory %s created in %s .\n", event->name, directory_name );
-          log_data_two("CREATED directory: ",  event->name );
-        }
-        else {
-          //printf( "New file %s created in %s .\n", event->name, directory_name );
-          log_data_two("CREATED file: ",  event->name );
-        }
-      }
-      else if ( event->mask & IN_DELETE ) {
-        if ( event->mask & IN_ISDIR ) {
-          //printf( "Directory %s deleted in %s .\n", event->name,directory_name  );
-          log_data_two("DElETED directory: ",  event->name );
-        }
-        else {
-          //printf( "File %s deleted %s .\n", event->name, directory_name );
-          log_data_two("DELETED file: ",  event->name );
+  /* do it forever*/
+  while(1)
+    {
+      i = 0;
+      length = read( fd, buffer, BUF_LEN ); 
+ 
+      if ( length < 0 ) {
+        perror( "read" );
+      } 
+ 
+      /* Read the events*/
+      while ( i < length ) {
+        struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];
+        if ( event->len ) {
+          if ( event->mask & IN_CREATE) {
+            if (event->mask & IN_ISDIR)
+              fprintf(fp_log,"%d DIR::%s CREATED\n", event->wd,event->name );      
+            else
+              fprintf(fp_log, "%d FILE::%s CREATED\n", event->wd, event->name);      
+          }
+           
+          if ( event->mask & IN_MODIFY) {
+            if (event->mask & IN_ISDIR)
+              fprintf(fp_log,"%d DIR::%s MODIFIED\n", event->wd,event->name );      
+            else
+              fprintf(fp_log,"%d FILE::%s MODIFIED\n", event->wd,event->name );      
+ 
+          }
+           
+          if ( event->mask & IN_DELETE) {
+            if (event->mask & IN_ISDIR)
+              fprintf(fp_log,"%d DIR::%s DELETED\n", event->wd,event->name );      
+            else
+              fprintf(fp_log,"%d FILE::%s DELETED\n", event->wd,event->name );      
+          } 
+ 
+          i += EVENT_SIZE + event->len;
         }
       }
     }
-    i += EVENT_SIZE + event->len;
-  }
+  /* Clean up*/
+  ( void ) close( fd );
+   
+  return 0;
 }
--- Assignment1/file_monitor.c
+++ Assignment1/file_monitor.c
@@ -1,24 +1,68 @@
-/* Using Inotify to monitor the sub-dirs under the specifiied dir*/
- 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <errno.h>
+#include <unistd.h>
+#include <signal.h>
 #include <sys/types.h>
-#include <linux/inotify.h>
+#include <sys/stat.h>
+#include <syslog.h>
+#include <errno.h>
+#include <string.h>
+#include <time.h>
 #include <dirent.h>
-#include <limits.h>
- 
-#define MAX_LEN 1024 /*Path length for a directory*/
-#define MAX_EVENTS 1024 /*Max. number of events to process at one go*/
-#define LEN_NAME 16 /*Assuming that the length of the filename won't exceed 16 bytes*/
-#define EVENT_SIZE  ( sizeof (struct inotify_event) ) /*size of one event*/
-#define BUF_LEN     ( MAX_EVENTS * ( EVENT_SIZE + LEN_NAME )) /*buffer to store the data of events*/
- 
- 
-/* Log file*/
-FILE *fp_log;
 
+#include <linux/inotify.h>
+
+#include "logger.h"
+
+#define EVENT_SIZE (sizeof (struct inotify_event))
+#define EVENT_BUF_LEN (1024 * (EVENT_SIZE + 16))
+
+void add_watches(int fd, char *root)
+{
+  int wd;
+  char *abs_dir;
+  struct dirent *entry;
+  DIR *dp;
+ 
+  dp = opendir(root);
+  if (dp == NULL)
+    {
+      perror("Error opening the starting directory");
+      exit(0);
+    }
+ 
+  /* add watch to starting directory */
+  wd = inotify_add_watch(fd, root, IN_CREATE | IN_MODIFY | IN_DELETE);
+  if (wd == -1)
+    {
+      fprintf(fp_log,"Couldn't add watch to %s\n",root);
+    }
+  else
+    {
+      printf("Watching:: %s\n",root);
+    }
+ 
+  /* Add watches to the Level 1 sub-dirs*/
+  abs_dir = (char *)malloc(MAX_LEN);
+  while((entry = readdir(dp)))
+    {
+      /* if its a directory, add a watch*/
+      if (entry->d_type == DT_DIR)
+        {
+          strcpy(abs_dir,root);
+          strcat(abs_dir,entry->d_name);
+           
+          wd = inotify_add_watch(fd, abs_dir, IN_CREATE | IN_MODIFY | IN_DELETE);
+          if (wd == -1)
+              printf("Couldn't add watch to the directory %s\n",abs_dir);
+          else
+            printf("Watching:: %s\n",abs_dir);
+        }
+    }
+   
+  closedir(dp);
+  free(abs_dir);
+}
 static void skeleton_daemon() {
 	pid_t pid;
 
@@ -73,129 +117,196 @@ static void skeleton_daemon() {
 	{
 		close (x);
 	}
-}
- 
-/* Add inotify watches to directories immediately under root
- * in addition to itself */
- 
-void add_watches(int fd, char *root)
-{
-  int wd;
-  char *abs_dir;
-  struct dirent *entry;
-  DIR *dp;
- 
-  dp = opendir(root);
-  if (dp == NULL)
-    {
-      perror("Error opening the starting directory");
-      exit(0);
-    }
- 
-  /* add watch to starting directory */
-  wd = inotify_add_watch(fd, root, IN_CREATE | IN_MODIFY | IN_DELETE);
-  if (wd == -1)
-    {
-      fprintf(fp_log,"Couldn't add watch to %s\n",root);
-    }
-  else
-    {
-      printf("Watching:: %s\n",root);
-    }
- 
-  /* Add watches to the Level 1 sub-dirs*/
-  abs_dir = (char *)malloc(MAX_LEN);
-  while((entry = readdir(dp)))
-    {
-      /* if its a directory, add a watch*/
-      if (entry->d_type == DT_DIR)
-        {
-          strcpy(abs_dir,root);
-          strcat(abs_dir,entry->d_name);
-           
-          wd = inotify_add_watch(fd, abs_dir, IN_CREATE | IN_MODIFY | IN_DELETE);
-          if (wd == -1)
-              printf("Couldn't add watch to the directory %s\n",abs_dir);
-          else
-            printf("Watching:: %s\n",abs_dir);
-        }
-    }
-   
-  closedir(dp);
-  free(abs_dir);
-}
- 
-/* Main routine*/
-int main( int argc, char **argv )
-{
-  int length, i = 0;
-  int fd;
-  char buffer[BUF_LEN], root[MAX_LEN];
-        
-  strcpy(root,"/root/logs/");     
-    
- 
-  /* Set up logger*/
-  fp_log = fopen("inotify_logger.log","a");
-  if (fp_log == NULL)
-    {
-      printf("Error opening logger. All output will be redirected to the stdout\n");
-      fp_log = stdout;
-    }
- 
-  fd = inotify_init();
-  if ( fd < 0 ) {
-    perror( "Couldn't initialize inotify");
-  }
- 
-  /* Read the sub-directories at one level under argv[1]
-   * and monitor them for access */
-  add_watches(fd,root);
 
-  skeleton_daemon();
+	/* Open the log file */
+	openlog ("firstdaemon", LOG_PID, LOG_DAEMON);
+}
 
-  /* do it forever*/
-  while(1)
-    {
-      i = 0;
-      length = read( fd, buffer, BUF_LEN ); 
- 
-      if ( length < 0 ) {
-        perror( "read" );
-      } 
- 
-      /* Read the events*/
-      while ( i < length ) {
-        struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];
-        if ( event->len ) {
-          if ( event->mask & IN_CREATE) {
-            if (event->mask & IN_ISDIR)
-              fprintf(fp_log,"%d DIR::%s CREATED\n", event->wd,event->name );      
-            else
-              fprintf(fp_log, "%d FILE::%s CREATED\n", event->wd, event->name);      
-          }
-           
-          if ( event->mask & IN_MODIFY) {
-            if (event->mask & IN_ISDIR)
-              fprintf(fp_log,"%d DIR::%s MODIFIED\n", event->wd,event->name );      
-            else
-              fprintf(fp_log,"%d FILE::%s MODIFIED\n", event->wd,event->name );      
- 
-          }
-           
-          if ( event->mask & IN_DELETE) {
-            if (event->mask & IN_ISDIR)
-              fprintf(fp_log,"%d DIR::%s DELETED\n", event->wd,event->name );      
-            else
-              fprintf(fp_log,"%d FILE::%s DELETED\n", event->wd,event->name );      
-          } 
- 
-          i += EVENT_SIZE + event->len;
-        }
-      }
-    }
-  /* Clean up*/
-  ( void ) close( fd );
-   
-  return 0;
+int main(int argc, char *argv[]) {
+	
+	int length, i = 0;
+	int fd;
+
+	int wd;
+
+	char buffer[EVENT_BUF_LEN];
+
+	skeleton_daemon();
+
+	//printf("the argv[] after daemon is %s",argv[1]);
+
+	syslog (LOG_NOTICE, "------------  First testing daemon started.----------");
+
+	/*creating the INOTIFY instance*/
+	fd = inotify_init();
+
+	/*checking for error*/
+	if ( fd < 0 ) {
+	  perror( "inotify_init" );
+	}
+
+	add_watches(fd,"/root/html/");
+
+	if(wd<0){
+	    syslog(LOG_NOTICE, "wd < 0");
+	    perror("inotify_add_watch");    
+	}
+
+	while (1)
+	{  		
+	    /*read to determine the event change happens on directory. Actually this read blocks until the change event occurs*/
+	    syslog (LOG_NOTICE, "BEFORE READ IN BUFFER");
+		length = read( fd, buffer, EVENT_BUF_LEN );
+
+	    syslog (LOG_NOTICE, "AFTER READ IN BUFFER.");
+
+		/*checking for error*/
+		if ( length < 0 ) {
+		    perror( "read" );
+		}else if(length == 0) {
+		    syslog(LOG_NOTICE," length =0 " );
+		    continue;
+	    }
+
+	/*actually read return the list of change events happens. Here, read the change event one by one and process it accordingly.*/
+	while ( i < length ) {     
+		struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];     
+		if ( event->len ) {
+
+		if (event->mask & IN_IGNORED) {
+				//inotify_rm_watch(fd,wd);
+				//inotify_add_watch(fd,"/root/html", IN_ALL_EVENTS );
+			}       
+
+		if ( event->mask & IN_ATTRIB ) {
+					if ( event->mask & IN_ISDIR ) {
+						//printf( " directory %s ATTRIB .\n", event->name );
+
+			            syslog (LOG_NOTICE, " directory:chaning     ATTRIB .");
+						syslog (LOG_NOTICE, " directory:%s ATTRIB .", event->name);
+                        log_data_two("ATTRIB: directory: ",  event->name );
+					}   
+					else {
+			            syslog (LOG_NOTICE, " directory:chaning     ATTRIB .");
+
+						syslog (LOG_NOTICE, " file:%s ATTRIB .", event->name); 
+                        log_data_two("ATTRIB: file: ",  event->name );                           
+					}   
+				}  
+
+		if ( event->mask & IN_MODIFY ) {
+				   if ( event->mask & IN_ISDIR ) {
+						//printf( " directory %s modified .\n", event->name );
+			            syslog (LOG_NOTICE, " dirtory:chaning    !!!!!!! .");
+						syslog (LOG_NOTICE, " directory:%s modify.", event->name);
+                        log_data_two("MODIFIED file: ",  event->name );
+					}
+					else {
+			            syslog (LOG_NOTICE, " file  modify:chaning!!! .");
+						syslog (LOG_NOTICE, " file:%s modify.", event->name);
+
+                        log_data_two("MODIFIED file: ",  event->name );
+					}
+				}
+
+		if ( event->mask & IN_CLOSE_WRITE ) {
+					if ( event->mask & IN_ISDIR ) {
+						//printf( " directory %s modified .\n", event->name );
+						syslog (LOG_NOTICE, " CLSOE WRITE    !!!!!!! .");
+						//syslog (LOG_NOTICE, " file:%s modify.", event->name);
+                        log_data_two("CLOSE directory: ",  event->name );
+					}
+					else {
+						//syslog (LOG_NOTICE, " file modify:changing!!! .");
+					    syslog (LOG_NOTICE, " CLSOE WRITE    !!!!!!! .");  
+                        log_data_two("CLOSE file: ",  event->name );                 
+						//syslog (LOG_NOTICE, " file:%s modify.", event->name);
+					}
+				}
+
+		if ( event->mask & IN_OPEN ) {
+					if ( event->mask & IN_ISDIR ) {
+						//printf( "New directory %s opened.\n", event->name );
+						syslog (LOG_NOTICE, " directory:%s open.", event->name);
+                        log_data_two("OPEN directory: ",  event->name );  
+					}
+					else {
+			            syslog (LOG_NOTICE, " file:opening.");
+                
+						syslog (LOG_NOTICE, " file:%s open.", event->name);
+						//printf( "New file %s open.\n", event->name );
+
+                        log_data_two("OPEN file: ",  event->name );  
+					}
+				}
+
+			if ( event->mask & IN_CREATE ) {
+			if ( event->mask & IN_ISDIR ) {
+				//printf( "New directory %s created.\n", event->name );
+				syslog (LOG_NOTICE, "new directory:%s created.", event->name);
+                log_data_two("NEW directory: ",  event->name );  
+			}
+			else {
+				syslog (LOG_NOTICE, "new file:%s created.", event->name);
+				//printf( "New file %s created.\n", event->name );
+                log_data_two("NEW file: ",  event->name );  
+
+				}
+			}
+			else if ( event->mask & IN_DELETE ) {
+			if ( event->mask & IN_ISDIR ) {
+				syslog (LOG_NOTICE, "dir:%s is deleted.", event->name);
+				//printf( "Directory %s deleted.\n", event->name );
+                log_data_two("DELETED directory: ",  event->name );  
+			}
+			else {
+				syslog (LOG_NOTICE, "file:%s is deleted.", event->name);
+				//printf( "File %s deleted.\n", event->name );
+                log_data_two("DELETED file: ",  event->name );  
+			}
+			}
+
+		else if ( event->mask & IN_ACCESS ) {
+					if ( event->mask & IN_ISDIR ) {
+						syslog(LOG_NOTICE, "dir:%s is accessed.", event->name);
+						//printf( "The directory %s was accessed.\n", event->name );
+                        log_data_two("ACCESSED directory: ",  event->name );  
+					}
+					else {
+						syslog(LOG_NOTICE, "file:%s is accessed.", event->name);
+						//printf( "The file %s was accessed.\n", event->name );
+                        log_data_two("ACCESSED file: ",  event->name );  
+					}   
+				} 
+
+		if ( event->mask & IN_CLOSE ) {
+					if ( event->mask & IN_ISDIR ) {
+						//printf( "New directory %s created.\n", event->name );
+						syslog (LOG_NOTICE, " directory:%s closed.", event->name);
+
+                        log_data_two("CLOSED directory: ",  event->name ); 
+					}
+					else {
+						syslog (LOG_NOTICE, " file:%s closed.", event->name);
+						//printf( "New file %s created.\n", event->name );
+                        log_data_two("CLOSED file: ",  event->name ); 
+
+					}
+				}
+
+			}
+		i += EVENT_SIZE + event->len;
+	}
+
+		i=0;    
+	    syslog (LOG_NOTICE, "~~~~~~one testing daemon ended.~~~~~~~~~~");
+		sleep (20);
+	}
+
+	/*removing the directory from the watch list.*/
+	inotify_rm_watch( fd, wd );
+	
+	syslog (LOG_NOTICE, "--------------- first testing daemon terminated.----------------");
+	closelog();
+	return EXIT_SUCCESS;
 }
