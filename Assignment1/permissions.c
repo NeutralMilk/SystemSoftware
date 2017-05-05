21bf726b2f7b23e851b78b30ec276d0433f88d57
Timothy Barnard <timothy.barnard@mydit.ie>
2017-05-05 14:27:59 +0100

    stystem updated

--- Assignment1/permissions.c
+++ Assignment1/permissions.c
@@ -6,46 +6,61 @@
 #include "permissions.h"
 #include "logger.h"
 
+
 int lockAllFiles(char* path) {
-    int status;
-    struct stat buffer;
-    struct stat st;
+    // int status;
+    // struct stat buffer;
+    // struct stat st;
 
-    mode_t mode;
-	stat(path, &st);
+    // mode_t mode;
+	// stat(path, &st);
 
-	mode = st.st_mode & 11111;
-	mode &= ~(S_IRUSR); 
-	mode |= S_IXUSR;  
-
-    chmod(path, mode);
-    status = stat(path, &buffer);
+	// mode = st.st_mode & 11111;
+	// mode &= ~(S_IRUSR); 
+	// mode |= S_IXUSR;  
+    //status = stat(path, &buffer);
 
     logInfoMessages("locking folder", path);
 
-    return status;
+    int value = 0;
+	char mode[] = "1111";
+	int i;
+	i = strtol(mode, 0, 8);
+	if ( chmod(path, i) < 0)
+	{
+		return 0;
+	}
+	return 1;
 }
 
 int unlockAllFiles(char* path) {
-    int status;
-    struct stat buffer;
-    struct stat st;
+
+    // struct stat buffer;
+    // struct stat st;
 
     //sleep(10);
 
-    mode_t mode;
-	stat(path, &st);
+    // mode_t mode;
+	// stat(path, &st);
 
-	mode = st.st_mode & 07777;
-	mode &= ~(S_IRUSR); 
-	mode |= S_IXUSR;  
+	// mode = st.st_mode & 07777;
+	// mode &= ~(S_IRUSR); 
+	// mode |= S_IXUSR;  
 
-    chmod(path, mode);
-    status = stat(path, &buffer);
+    // chmod(path, mode);
+    // status = stat(path, &buffer);
 
     logInfoMessages("unlocking folder", path);
     
-    return status;
+    int value = 0;
+	char mode[] = "07777";
+	int i;
+	i = strtol(mode, 0, 8);
+	if ( chmod(path, i) < 0)
+	{
+		return 0;
+	}
+	return 1;    
 }
 
 // int main(int argc, char *argv[]) {
